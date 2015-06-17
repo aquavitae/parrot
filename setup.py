2492b3403732dcbe2f7bcdda8b27f331804fe586
David Townshend <david.townshend@aurecongroup.com>
2015-06-17 08:58:20 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Parrot',
+    version='0.1',
+    description='Twitter-like feed parser',
+    author='David Townshend',
+    url='https://github.com/aquavitae/parrot',
+    # py_modules=['parrot'],
+    scripts=['parrot.py'],
+)
